d3520f8ee0a783dc7d1b19e10397f2a07cd9fff8
Zolyn <zorin1477@gmail.com>
2022-07-31 22:45:10 +0800

    build(tsup): update config

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,9 +6,7 @@ export default defineConfig({
   minify: true,
   format: ['esm', 'cjs'],
   entry: ['./src/index.ts', './src/nuxt.ts'],
-  target: 'node14',
   clean: true,
-  external: ['fast-glob', '@babel/types', 'local-pkg'],
   dts: isProduction,
   esbuildOptions(options) {
     if (isProduction) {
