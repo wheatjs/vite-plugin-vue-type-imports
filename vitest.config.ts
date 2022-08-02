6c87aafe20ac0a766a27b20486eac46e6838da66
Zolyn <zorin1477@gmail.com>
2022-08-03 01:37:15 +0800

    chore: remove path aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vitest/config';
-import { resolve } from 'node:path';
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '~': resolve(__dirname, 'src')
-        }
-    },
     test: {
         coverage: {
             reporter: ['text', 'json', 'html']
