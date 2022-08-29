1df93fb4d65f9badccfee2b83f6ae6c65845492c
Zolyn <zorin1477@gmail.com>
2022-08-29 08:23:36 +0800

    fix(tests): specify transform target
    ci: update

--- src/core/ast.ts
+++ src/core/ast.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import fs from 'fs'
 import type {
   CallExpression,
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  esbuild: {
+    target: 'node14',
+  },
   test: {
     coverage: {
       reporter: ['text', 'json', 'html'],
