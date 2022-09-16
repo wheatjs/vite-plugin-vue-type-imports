cc160cefb6f9c526817b68ca9c11ae7d2eb05243
nooooooom <nooooooom@163.com>
2022-09-16 14:15:39 +0800

    test: add test

---
+++ test/fixtures/dynamic/tsx/default/index.vue
@@ -0,0 +1,10 @@
+<script lang="tsx" setup>
+interface Props {
+  foo: string
+}
+
+defineProps<Props>()
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const render = () => <div />
+</script>
