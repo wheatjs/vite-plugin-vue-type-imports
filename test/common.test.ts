43e39dd21a973719af466e845a2086e68f93dd98
Zolyn <zorin1477@gmail.com>
2022-09-18 13:07:36 +0800

    fix!: remove `.ts` extension && drop support for importing type files with extensions

--- test/common.test.ts
+++ test/common.test.ts
@@ -1,8 +1,9 @@
+import { removeTSExtension } from '../src/core/utils'
 import type { CodeGetter } from './_utils'
 import { defineTransformTest } from './_utils'
 
 const codeGetter: CodeGetter = ({ entry }) => `<script lang="ts" setup>
-import { Props } from '${entry}'
+import { Props } from '${removeTSExtension(entry)}'
 
 defineProps<Props>()
 </script>
